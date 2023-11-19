a94894d08ccdc02d4cf2345ac71e3739a661a78b
victor-jvas <victor.jvas1@gmail.com>
2023-11-19 14:49:58 -0300

    Ability System Componentes and Player State

---
+++ Source/TalesOfObsydia/Private/Player/BasePlayerState.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/BasePlayerState.h"
+
+ABasePlayerState::ABasePlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/TalesOfObsydia/Public/Player/BasePlayerState.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "BasePlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TALESOFOBSYDIA_API ABasePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+
+	ABasePlayerState();
+	
+};
