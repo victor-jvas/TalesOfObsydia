d37ad4522290a2bc940ba9ded205c01f52465125
VictorJ <70246654+victor-jvas@users.noreply.github.com>
2023-11-19 00:33:20 -0300

    Delete Source directory

--- Source/CombatTry/CombatTry.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "CombatTry.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CombatTry, "CombatTry" );
